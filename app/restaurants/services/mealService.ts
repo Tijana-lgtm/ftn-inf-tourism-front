eefd17415c4d6236fcdcccec3c58c7ba6f0bcd34
Danilo <danilokuzman1@yahoo.com>
2025-11-20 17:33:32 +0100

    feature/meals-management:dodavanje jela

---
+++ app/restaurants/services/mealService.ts
@@ -0,0 +1,67 @@
+import {Meal} from "../models/meal";
+
+export class MealService{
+  private apiUrl: string;
+
+  constructor(){
+    this.apiUrl = 'http://localhost:5105/api/restaurants';
+  }
+
+  getAll(restaurantId: string): Promise<Meal[]> {
+          return fetch(`${this.apiUrl}/${restaurantId}/meals`)
+              .then(response => {
+                  if (!response.ok) {
+                      return response.text().then(errorMessage => {
+                          throw { status: response.status, message: errorMessage }
+                      })
+                  }
+                  return response.json()
+              })
+              .then((meals: Meal[]) => {
+                  return meals;
+              })
+              .catch(error => {
+                  console.error('Error:', error.status)
+                  throw error
+              });
+      }
+      
+  add(restaurantId: string, formData: Meal): Promise<Meal> {
+          return fetch(`${this.apiUrl}/${restaurantId}/meals`, {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(formData)
+          })
+              .then(response => {
+                  if (!response.ok) {
+                      return response.text().then(errorMessage => {
+                          throw { status: response.status, message: errorMessage }
+                      })
+                  }
+                  return response.json()
+              })
+              .then((meal: Meal) => {
+                  return meal
+              })
+              .catch(error => {
+                  console.error('Error:', error.status)
+                  throw error
+              })
+      }
+
+  delete(restaurantId: string, mealId: string): Promise<void>{
+    return fetch(`${this.apiUrl}/${restaurantId}/meals/${mealId}`, {method: 'DELETE'})
+      .then(response =>{
+        if(!response.ok){
+          return response.text().then(errorMessage =>{
+            throw{status: response.status, message: errorMessage}
+          })
+        }
+      })
+      .catch(error =>{
+        console.error(error.status)
+        throw error
+      });
+    }
+      
+}
